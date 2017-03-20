56961674eb78af86925b46d1951d1d0ee67aaaf7
zimny.krzysztof <180318@edu.p.lodz.pl>
2017-03-20 23:42:37 +0100

    [testy_v1] Dodano test case elementIsInSequence dla sequence.lenght() = 1 oraz key znajduje sie w sekwencji
    Signed-off-by: zimny.krzysztof <180318@edu.p.lodz.pl>

---
+++ src/test/java/BinarySearchTest.java
@@ -0,0 +1,19 @@
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by Krzysztof Zimny on 3/20/2017.
+ */
+public class BinarySearchTest {
+
+    @Test
+    public void elementIsInSequenceTest() {
+        int sequence[] = new int[]{2};
+        int key = 2;
+        SearchResult searchResult = BinarySearch.search(key, sequence);
+        Assert.assertTrue(searchResult.isFound());
+        Assert.assertTrue(searchResult.getPosition() == 1);
+    }
+}
