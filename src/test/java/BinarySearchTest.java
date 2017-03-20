b13fff18cfdd224d6bd8d2bc1378e67a1608f0a7
zimny.krzysztof <180318@edu.p.lodz.pl>
2017-03-20 23:45:04 +0100

    [testy_v1] Dodano test case elementIsNotInSequence dla sequence.lenght() = 1 oraz key nie znajduje sie w sekwencji
    Signed-off-by: zimny.krzysztof <180318@edu.p.lodz.pl>

--- src/test/java/BinarySearchTest.java
+++ src/test/java/BinarySearchTest.java
@@ -16,4 +16,13 @@ public class BinarySearchTest {
         Assert.assertTrue(searchResult.isFound());
         Assert.assertTrue(searchResult.getPosition() == 1);
     }
+
+    @Test
+    public void elementIsNotInSequenceTest() {
+        int sequence[] = new int[]{2};
+        int key = 1;
+        SearchResult searchResult = BinarySearch.search(key, sequence);
+        Assert.assertTrue(!searchResult.isFound());
+        Assert.assertTrue(searchResult.getPosition() == -1);
+    }
 }
